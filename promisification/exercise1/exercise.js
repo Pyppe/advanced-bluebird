1b150028dc1cb51a4d9bd7dfa26b63d16cf20a47
Pyry-Samuli Lahti <pyry-samuli.lahti@onomatics.com>
2015-09-25 11:02:38 +0300

    promisification ex. 1

--- promisification/exercise1/exercise.js
+++ promisification/exercise1/exercise.js
@@ -2,21 +2,24 @@ var fs = require("fs");
 var Promise = require("bluebird");
 
 Promise.promisifyAll(fs, {
-    filter: function(name, method, target, passesDefaultFilter) {
-        // Implementation: filter out 'exists'
-    }
+  filter: function(name, method, target, passesDefaultFilter) {
+    // Implementation: filter out 'exists'
+    return name !== 'exists';
+  }
 });
 
 Promise.promisifyAll(fs, {
-    filter: function(name, method, target, passesDefaultFilter) {
-        // Implementation: filter in 'exists'
-    },
+  filter: function(name, method, target, passesDefaultFilter) {
+    // Implementation: filter in 'exists'
+    return name === 'exists';
+  },
 
-    promisifier: function(fn) {
-        return function(fileName) {
-            return new Promise(function(resolve) {
-                fs.exists(fileName, resolve);
-            });
-        }
+  promisifier: function(fn) {
+    return function(fileName) {
+      return new Promise(function(resolve) {
+        // fs.exists === fn
+        fn(fileName, resolve);
+      });
     }
+  }
 });
