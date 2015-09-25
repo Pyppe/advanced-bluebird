1d7a55cfaa7af1ae518ae7a0e2ba6a46fd3b924f
Pyry-Samuli Lahti <pyry-samuli.lahti@onomatics.com>
2015-09-25 11:25:04 +0300

    promisification ex. 2

--- promisification/exercise2/exercise.js
+++ promisification/exercise2/exercise.js
@@ -3,18 +3,29 @@ var Promise = require("bluebird");
 var TimeoutError = Promise.TimeoutError;
 
 Promise.promisifyAll(restler, {
-    promisifier: function(originalFunction) {
-        return function promisifiedRestlerMethod() {
-            // Implementation:
-            //
-            // Call the original restler method with same context and arguments as
-            // this function was called and saved the returned restler request object
-            // to a variable.
-            //
-            // Return a Promise that will be:
-            // - Resolved when the restler request object gets "success" event
-            // - Rejected when the restler request object gets "fail" or "error" event
-            // - Rejected with TimeoutError when the restler request object gets "timeout" event
-        }
+  promisifier: function (originalFunction) {
+    return function promisifiedRestlerMethod() {
+      // Implementation:
+      //
+      // Call the original restler method with same context and arguments as
+      // this function was called and saved the returned restler request object
+      // to a variable.
+      //
+      // Return a Promise that will be:
+      // - Resolved when the restler request object gets "success" event
+      // - Rejected when the restler request object gets "fail" or "error" event
+      // - Rejected with TimeoutError when the restler request object gets "timeout" event
+
+      var restlerObj = originalFunction.apply(this, arguments);
+      return new Promise(function(resolve, reject) {
+        restlerObj.
+          on('success', resolve).
+          on('fail', reject).
+          on('error', reject).
+          on('timeout', function() {
+            reject(new TimeoutError());
+          });
+      });
     }
+  }
 });
